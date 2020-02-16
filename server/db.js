d06fd79c2ac1b912b87e959132590ce626c66a63
Roy Peled <peled.roy@gmail.com>
2020-02-16 20:07:32 +0200

    adde db env var

--- server/db.js
+++ server/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/users-list';
+
 async function setDb() {
-    await mongoose.connect('mongodb://localhost:27017/users-list', {
+    await mongoose.connect(dbUrl, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
